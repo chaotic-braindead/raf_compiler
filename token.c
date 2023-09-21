092707bd057e6db55558308a8bae300b0aeb4f0c
chaotic-braindead <rafaelsebastian0908@gmail.com>
2023-09-21 17:26:40 +0800

    improved readability of token.c

--- token.c
+++ token.c
@@ -13,31 +13,31 @@ token_t* tokenInit(char *buf, token_type type){
 // there's definitely a better way to do this but I already put in too much effort on this lmao
 char* tokenTypeToWords(token_type type){
     switch(type){
-        case 0 : return "TOKEN_ID";
-        case 1 : return "TOKEN_INT";
-        case 2 : return "TOKEN_LPAREN";
-        case 3 : return "TOKEN_RPAREN";
-        case 4 : return "TOKEN_LCBRACE";
-        case 5 : return "TOKEN_RCBRACE";
-        case 6 : return "TOKEN_LBRACE";
-        case 7 : return "TOKEN_RBRACE";
-        case 8 : return "TOKEN_COLON";
-        case 9 : return "TOKEN_SEMICOLON";
-        case 10 : return "TOKEN_SQOUTE";
-        case 11 : return "TOKEN_DQUOTE";
-        case 12 : return "TOKEN_PLUS";
-        case 13 : return "TOKEN_MINUS";
-        case 14 : return "TOKEN_ASTERISK";
-        case 15 : return "TOKEN_FSLASH";
-        case 16 : return "TOKEN_BSLASH";
-        case 17 : return "TOKEN_PERCENT";
-        case 18 : return "TOKEN_EQUALS";
-        case 19 : return "TOKEN_COMMA";
-        case 20 : return "TOKEN_LBRACKET";
-        case 21 : return "TOKEN_RBRACKET";
-        case 22 : return "TOKEN_ARROW";
-        case 23 : return "TOKEN_COMMENT";
-        case 24 : return "TOKEN_EOF";
+        case TOKEN_ID        : return "TOKEN_ID";
+        case TOKEN_INT       : return "TOKEN_INT";
+        case TOKEN_LPAREN    : return "TOKEN_LPAREN";
+        case TOKEN_RPAREN    : return "TOKEN_RPAREN";
+        case TOKEN_LCBRACE   : return "TOKEN_LCBRACE";
+        case TOKEN_RCBRACE   : return "TOKEN_RCBRACE";
+        case TOKEN_LBRACE    : return "TOKEN_LBRACE";
+        case TOKEN_RBRACE    : return "TOKEN_RBRACE";
+        case TOKEN_COLON     : return "TOKEN_COLON";
+        case TOKEN_SEMICOLON : return "TOKEN_SEMICOLON";
+        case TOKEN_SQUOTE    : return "TOKEN_SQUOTE";
+        case TOKEN_DQUOTE    : return "TOKEN_DQUOTE";
+        case TOKEN_PLUS      : return "TOKEN_PLUS";
+        case TOKEN_MINUS     : return "TOKEN_MINUS";
+        case TOKEN_ASTERISK  : return "TOKEN_ASTERISK";
+        case TOKEN_FSLASH    : return "TOKEN_FSLASH";
+        case TOKEN_BSLASH    : return "TOKEN_BSLASH";
+        case TOKEN_PERCENT   : return "TOKEN_PERCENT";
+        case TOKEN_EQUALS    : return "TOKEN_EQUALS";
+        case TOKEN_COMMA     : return "TOKEN_COMMA";
+        case TOKEN_LBRACKET  : return "TOKEN_LBRACKET";
+        case TOKEN_RBRACKET  : return "TOKEN_RBRACKET";
+        case TOKEN_ARROW     : return "TOKEN_ARROW";
+        case TOKEN_COMMENT   : return "TOKEN_COMMENT";
+        case TOKEN_EOF       : return "TOKEN_EOF";
     }
     return "TOKEN_UNPROCESSED";
 }
